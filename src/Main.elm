bb2e4ceb02a9ad9efb10622c1a16286ce843b444
kazuhi-ra <kazuhira4126@gmail.com>
2021-04-17 23:36:06 +0900

    型注釈を使ってみる2

--- src/Main.elm
+++ src/Main.elm
@@ -4,9 +4,24 @@ import Html exposing (text)
 
 
 main =
-    text greeting
+    text (Debug.toString meaningOfLife)
 
 
 greeting : String
 greeting =
     "Hello, Static Elm!"
+
+
+meaningOfLife : Int
+meaningOfLife =
+    42
+
+
+pi : Float
+pi =
+    3.14
+
+
+canChuck : Bool
+canChuck =
+    True
